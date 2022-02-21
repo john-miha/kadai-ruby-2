032af46c3f3ce9df8ac8404825a86f6f3e6d6012
john <sphyrna.job@gmail.com>
2022-02-21 08:01:19 +0000

    first commit

---
+++ kadai-ruby-2/kadai-fizzbuzz.rb
@@ -0,0 +1,17 @@
+num_max = 100
+
+def fizzbuzz(num)
+  if (num % 15) == 0
+    return "FizzBuzz"
+  elsif (num % 5) == 0
+    return "Buzz"
+  elsif (num % 3) == 0    
+    return "Fizz"
+  else
+    return num
+  end
+end
+
+(1..num_max).each do |num|
+  puts fizzbuzz(num)
+end
